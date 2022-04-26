7f145046c9708575a95ef317d150160c8c877c70
Misba Adeeb <Misba.Adeeb@ext.saint-gobain.com>
2022-04-26 14:41:28 +0530

    BinChange Implementation

---
+++ BcpApi/Controllers/Transactions/BinChangeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Bcp.Domain.Dtos;
+using System.Threading.Tasks;
+using Bcp.Api.Services.Common;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Bcp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BinChangeController : ControllerBase
+    {
+        private readonly ICommonService _commonService;
+        public BinChangeController(ICommonService commonService)
+        {
+            _commonService = commonService;
+        }
+
+        [HttpPost("SaveBinChanges")]
+        public async Task<IActionResult> SaveBinChanges(StockWmsDto binChanges)
+        {
+           await _commonService.SaveBinChanges(binChanges);
+            return Ok();
+        }
+    }
+}
---
+++ BcpApi/Services/Common/ICommonService.cs
@@ -0,0 +1,13 @@
+﻿using Bcp.Domain.Dtos;
+using Bcp.Domain.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bcp.Api.Services.Common
+{
+    public interface ICommonService
+    {
+        Task SaveBinChanges(StockWmsDto binChanges);
+
+    }
+}
